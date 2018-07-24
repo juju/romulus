f6cc7a9c41651bb51d876540335f620af571f5c6
Casey Marshall <me@cmars.tech>
2018-07-24 10:32:02 -0500

    godoc

--- default.go
+++ default.go
@@ -4,4 +4,5 @@
 // Package romulus contains service API clients related to metering & SLAs.
 package romulus
 
+// DefaultAPIRoot is the default URL prefix for metering & SLA API endpoints.
 var DefaultAPIRoot = "https://api.jujucharms.com/omnibus/v3"
